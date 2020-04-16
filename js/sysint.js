ca38bcc722383dd23b3eced2cbb8c701aa2b704c
sllq <axekote21@gmail.com>
2020-04-16 17:10:32 +0500

    init

---
+++ js/sysint.js
@@ -0,0 +1,6 @@
+document.writeln('<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>');
+document.writeln('<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>');
+// $('head').append('<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="crossorigin="anonymous"></script>');
+$( document ).ready(function() {
+
+});
